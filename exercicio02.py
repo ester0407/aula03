93f340a78e0cdd64b25758ce2bffe73e82f1217d
ester araujo <esteraraujosantos2006@gmail.com>
2025-03-31 09:52:40 -0300

    eviando exercicio02

---
+++ exercicio02.py
@@ -0,0 +1,14 @@
+numero01= int (input("digite o numero01"))
+numero02= int (input("digite o numer02"))
+
+soma= numero01+numero02
+subtracao= numero01-numero02
+multiplicacao= numero01*numero02
+divisao= numero01/numero02
+
+print(f"a soma {numero01} + {numero02} = {soma}")
+print(f"a subtracao { numero01} - {numero02} = {subtracao}")
+print(f"a multiplicacao {numero01} * {numero02} = {multiplicacao} ")
+print(f" a divisao  {numero01} / {numero02} = {divisao}")
+
+
