22dfe1af914d2a705f4034e08a29b304c62f44d0
ester araujo <esteraraujosantos2006@gmail.com>
2025-03-31 11:49:08 -0300

    enviando exercicio06

---
+++ exercicio5.py
@@ -0,0 +1,14 @@
+time1= (input("digite o primeiro time"))
+ngols1= int(input("digite o numero de gols"))
+time2 =(input("digite o segundo time"))
+ngols2= int(input("digite o numero dee gols"))
+if ngols1=ngols2:
+    print(f" empate")
+else:
+   if ngols1>ngols2
+        print(f"0 {time1} ganhou")
+
+else:
+        print(f" o {time2} ganhou")
+
+
