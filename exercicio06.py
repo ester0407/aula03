ed0630665617900f58f65dc3834fc35b03a186f2
ester araujo <esteraraujosantos2006@gmail.com>
2025-04-02 09:28:51 -0300

    enviando continuação exercicio06

--- exercicio06.py
+++ exercicio06.py
@@ -4,11 +4,12 @@ valor_da_gasolina= 5.80
 valor_do_etanol= 4.90
 valor_total=0
 
-if tipos== "g":
+if tipos== "g"
     print(f"voce escolheu gasolina {litros}")
     valor_total= valor_da_gasolina*litros
     print(f"valor total de: R$ {valor_total}")
 else:
+
     print(f"voce escolheu etanol {litros}")
     valor_total= valor_do_etanol*litros
     print(f"seu valor_total de R$ {valor_total}")
